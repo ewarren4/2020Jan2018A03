2a50b9d5f6441747745b8b9766f3a3c41198a921
ewarren4 <54917628+ewarren4@users.noreply.github.com>
2020-03-02 13:51:29 -0700

    .

---
+++ ChinookSolution/ChinookSystem/BLL/TrackController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.Data.DTOs;
+using ChinookSystem.Data.POCOs;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class TrackController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<TrackList> Track_GetTracksByTypeandID(string tracksby, string searcharg)
+        {
+            using(var context = new ChinookContext())
+            {
+                List<TrackList> results = null;
+                int searchint = 0;
+                if (int.TryParse(searcharg, out searchint))
+                {
+                    //searches for MediaType and Genre
+                    results = (from x in context.Tracks
+                              where (x.GenreId == searchint && tracksby.Equals("Genre"))
+                                || (x.MediaTypeId == searchint && tracksby.Equals("MediaType"))
+                              select new TrackList
+                              {
+                                   TrackID = x.TrackId,
+                                   Name = x.Name,
+                                   Title = x.Album.Title,
+                                   ArtistName = x.Album.Artist.Name,
+                                   MediaName = x.MediaType.Name,
+                                   GenreName = x.Genre.Name,
+                                   Composer = x.Composer,
+                                   Milliseconds = x.Milliseconds,
+                                   Bytes = x.Bytes,
+                                   UnitPrice = x.UnitPrice
+                              }).ToList();
+
+                }
+                else
+                {
+                    //searches for Artist and Album
+                    results = (from x in context.Tracks
+                               where (x.Album.Title.Contains(searcharg) && tracksby.Equals("Album"))
+                                 || (x.Album.Artist.Name.Contains(searcharg) && tracksby.Equals("Artist"))
+                               select new TrackList
+                               {
+                                   TrackID = x.TrackId,
+                                   Name = x.Name,
+                                   Title = x.Album.Title,
+                                   ArtistName = x.Album.Artist.Name,
+                                   MediaName = x.MediaType.Name,
+                                   GenreName = x.Genre.Name,
+                                   Composer = x.Composer,
+                                   Milliseconds = x.Milliseconds,
+                                   Bytes = x.Bytes,
+                                   UnitPrice = x.UnitPrice
+                               }).ToList();
+                }
+                return results;
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Track> Track_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Tracks.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Track Track_Find(int trackid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Tracks.Find(trackid);
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Track> Track_GetByAlbumId(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from aRowOn in context.Tracks
+                              where aRowOn.AlbumId.HasValue
+                              && aRowOn.AlbumId == albumid
+                              select aRowOn;
+                return results.ToList();
+            }
+        }
+
+       
+    }//eoc
+}
